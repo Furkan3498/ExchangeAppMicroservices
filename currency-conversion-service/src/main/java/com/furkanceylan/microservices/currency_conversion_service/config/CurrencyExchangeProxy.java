03ffa386eb6f75ccc7669340adf9c77672c8dd14
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-03-17 17:29:10 +0300

    load balancing

--- currency-conversion-service/src/main/java/com/furkanceylan/microservices/currency_conversion_service/config/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/furkanceylan/microservices/currency_conversion_service/config/CurrencyExchangeProxy.java
@@ -7,12 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange"
-, url = "localhost:8000")
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
 
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    public ResponseEntity<CurrencyConversion> retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+     ResponseEntity<CurrencyConversion> retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
 }
