4a9c7017356b1259e0146ef2b6f58ce0fc446b20
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-04-20 22:57:47 +0300

    api-gateway

--- api-gateway/src/main/java/com/furkanceylan/microservices/api_gateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/furkanceylan/microservices/api_gateway/ApiGatewayConfiguration.java
@@ -1,13 +1,19 @@
 package com.furkanceylan.microservices.api_gateway;
 
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
 
 @Configuration
 public class ApiGatewayConfiguration {
 
     public RouteLocator gatewayRouter(RouteLocatorBuilder routeLocatorBuilder){
-        return null;
+        Function<PredicateSpec, Buildable<Route>> routeFunction = predicateSpec -> predicateSpec.path("/get").uri("http://httpbin.org:80");
+        return routeLocatorBuilder.routes().route(routeFunction).build();
     }
 }
