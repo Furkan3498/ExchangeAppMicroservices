84d62eb49f78476f77e2f955c22824e760bf8fd2
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-05-23 23:49:27 +0300

    Fault Tolerance -circuitbreaker-resilience4

--- currency-exchange-service/src/main/java/com/furkanceylan/microservices/currency_exchange_service/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/furkanceylan/microservices/currency_exchange_service/controller/CircuitBreakerController.java
@@ -1,14 +1,21 @@
 package com.furkanceylan.microservices.currency_exchange_service.controller;
 
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
+    //@Retry()
     public String sampleApi(){
-        return "Sample Api";
+
+        ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("localhost:8080/some-dummy-url",String.class);
+
+            return responseEntity.getBody();
     }
 }
