326f80c64f35a3499c86ddc1914f9a96d9932001
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-05-23 23:57:28 +0300

    Fault Tolerance -circuitbreaker-resilience4

--- currency-exchange-service/src/main/java/com/furkanceylan/microservices/currency_exchange_service/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/furkanceylan/microservices/currency_exchange_service/controller/CircuitBreakerController.java
@@ -2,6 +2,8 @@ package com.furkanceylan.microservices.currency_exchange_service.controller;
 
 
 import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,12 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
-
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     @GetMapping("/sample-api")
-    //@Retry()
+    @Retry(name = "default")
     public String sampleApi(){
-
-        ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("localhost:8080/some-dummy-url",String.class);
+        logger.info("Sample api call received");
+        ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",String.class);
 
             return responseEntity.getBody();
     }
