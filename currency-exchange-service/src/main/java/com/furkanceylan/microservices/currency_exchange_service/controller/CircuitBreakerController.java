9f47a51d6d08a7b058e66128ce31aca7284aac7c
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-05-30 22:45:36 +0300

    @RateLimiter

--- currency-exchange-service/src/main/java/com/furkanceylan/microservices/currency_exchange_service/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/furkanceylan/microservices/currency_exchange_service/controller/CircuitBreakerController.java
@@ -1,6 +1,7 @@
 package com.furkanceylan.microservices.currency_exchange_service.controller;
 
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
@@ -14,8 +15,9 @@ import org.springframework.web.client.RestTemplate;
 public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    //@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
     @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String sampleApi(){
         logger.info("Sample api call received");
       /*  ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",String.class);
